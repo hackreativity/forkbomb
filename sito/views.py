d874eb572d161c8008553234ed203b8b16872599
tapionx <tapion@tapion.it>
2016-10-30 01:18:31 +0200

    Mappa

--- sito/views.py
+++ sito/views.py
@@ -38,11 +38,19 @@ def livemap(request):
 
     impianti = Impianto.objects.filter(pubblicato=True)
     impianti_json = json.dumps(
-        list(impianti.values('id', 'cliente', 'prodotto', 'indirizzo', 'citta', 'lat', 'lon', 'resa_specifica', 'immagine', 'prodotto__tipologia__nome'))
+        list(impianti.values('id', 'cliente', 'prodotto__nome', 'indirizzo', 'citta', 'lat', 'lon', 'resa_specifica', 'immagine', 'prodotto__tipologia__nome'))
     )
     tipologie = Tipologia.objects.all()
+
+    potenza_totale = 0
+    for i in impianti:
+        potenza_totale += float(i.resa_specifica)
+    potenza_totale = int(potenza_totale)
+    potenza_totale = "{:,}".format(potenza_totale)
+
     return render(request, 'livemap.html', {
         'impianti': impianti,
         'impianti_json': impianti_json,
         'tipologie': tipologie,
+        'potenza_totale': potenza_totale,
     })
